a171185deb10187fccb93acc3df5338ea167e383
JorikFat <jorikfat@gmail.com>
2024-02-03 13:07:04 +0300

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# dev_sources
+
+Jorik's development sources
+
+## include:
+
+- StubScreen
+- Kotlin like nullsafety
+
---
+++ android/app/src/main/kotlin/com/example/dev_sources/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dev_sources
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
